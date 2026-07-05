39152f0a468b02211d4bba0c942efd2065906ec9
nawakarit <272197659+nawakarit-VOID@users.noreply.github.com>
2026-07-05 21:42:53 +0700

    *แบบ WidgetRenderer

--- preview.go
+++ preview.go
@@ -4,6 +4,8 @@
 package main
 
 import (
+	"image/color"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/widget"
@@ -14,24 +16,82 @@ type Preview struct {
 
 	state *AppState
 
-	text *canvas.Text
+	bg     *canvas.Rectangle
+	border *canvas.Rectangle
+	label  *canvas.Text
 }
 
 func NewPreview(state *AppState) *Preview {
-
 	p := &Preview{
 		state: state,
-		text:  canvas.NewText("Preview", nil),
+		bg: canvas.NewRectangle(color.NRGBA{
+			R: 45,
+			G: 45,
+			B: 45,
+			A: 255,
+		}),
+		border: canvas.NewRectangle(color.Transparent),
+		label:  canvas.NewText("Preview Area", color.White),
 	}
+
+	p.border.StrokeColor = color.NRGBA{R: 160, G: 160, B: 160, A: 255}
+	p.border.StrokeWidth = 2
 
 	p.ExtendBaseWidget(p)
 
 	return p
-
 }
 
 func (p *Preview) CreateRenderer() fyne.WidgetRenderer {
+	objects := []fyne.CanvasObject{
+		p.bg,
+		p.border,
+		p.label,
+	}
 
-	return widget.NewSimpleRenderer(p.text)
+	return &previewRenderer{
+		preview: p,
+		objects: objects,
+	}
+}
 
+type previewRenderer struct {
+	preview *Preview
+	objects []fyne.CanvasObject
+}
+
+func (r *previewRenderer) Layout(size fyne.Size) {
+
+	r.preview.bg.Resize(size)
+
+	margin := float32(40)
+
+	r.preview.border.Move(fyne.NewPos(margin, margin))
+	r.preview.border.Resize(fyne.NewSize(
+		size.Width-margin*2,
+		size.Height-margin*2,
+	))
+
+	labelSize := r.preview.label.MinSize()
+
+	r.preview.label.Move(fyne.NewPos(
+		(size.Width-labelSize.Width)/2,
+		(size.Height-labelSize.Height)/2,
+	))
+}
+
+func (r *previewRenderer) MinSize() fyne.Size {
+	return fyne.NewSize(400, 300)
+}
+
+func (r *previewRenderer) Refresh() {
+	canvas.Refresh(r.preview.bg)
+	canvas.Refresh(r.preview.border)
+	canvas.Refresh(r.preview.label)
+}
+
+func (r *previewRenderer) Destroy() {}
+
+func (r *previewRenderer) Objects() []fyne.CanvasObject {
+	return r.objects
 }
